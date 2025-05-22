348ca2a640702bce0a878cf35825ab3dfa688f1e
MinhQuang2612 <minhquang.yi@gmail.com>
2025-05-22 13:59:08 +0700

    fix: api docker

--- CoffeeShopApp/database/user-service/src/routes/userRoutes.js
+++ CoffeeShopApp/database/user-service/src/routes/userRoutes.js
@@ -5,12 +5,12 @@ const userController = require('../controllers/userController');
 router.post('/login', userController.login);
 router.post('/auth/google', userController.loginGoogle);
 router.post('/auth/facebook', userController.loginFacebook);
-router.get('/:userId', userController.getUserById);
-router.put('/:userId', userController.updateUser);
+router.get('/user/:userId', userController.getUserById);
+router.put('/user/:userId', userController.updateUser);
 router.put('/change-password/:userId', userController.changePassword);
-router.post('/send-otp',userController.sendOtp);
-router.post('/create-account',userController.createAcount);
-router.post('/check-account',userController.checkAccount);
-router.post('/reset-password', userController.resetPassword);
+router.post('/user/send-otp',userController.sendOtp);
+router.post('/user/create-account',userController.createAcount);
+router.post('/user/check-account',userController.checkAccount);
+router.post('/user/reset-password', userController.resetPassword);
 
 module.exports = router; 
